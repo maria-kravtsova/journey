f7c472d74ecf129cf32e331d38b6996d330fa7d2
Mariia Kravtsova <mariia@quikly.com>
2018-08-22 23:40:00 +0000

    Add components from tutorial

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,14 @@
 import React from "react"
+import Link from "gatsby-link"
 
-export default () => <div>Hello world!</div>
+export default () =>
+  <div style={{ color: 'tomato'}}>
+    Hello world!
+    <br />
+    <div>
+      <Link to="/page-2">Link</Link>
+    </div>
+    <div>
+      <Link to="/counter">Counter</Link>
+    </div>
+  </div>
